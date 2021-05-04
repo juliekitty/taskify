94c7e23024d9d71516f76ae7152b3262d4b651f5
Julie Landry <julie.landry@gmail.com>
2021-05-04 10:31:55 +0200

    add playTaskview

---
+++ taskify/taskify/Views/PlayTaskView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct PlayTaskView: View {
+  //  @Binding var showPlay: Bool
+    
+    var body: some View {
+        /* NavigationLink(destination: MainTasksList(showForm: showForm, tasksViewModel: tasksViewModel) {
+         EmptyView()
+         }*/
+        
+        
+        ZStack {
+            Color(UIColor.cyan)
+                .navigationTitle("Play my tasks")
+                .navigationBarTitleDisplayMode( .large)
+                .cornerRadius(10)
+                .padding(20)
+            
+            
+            
+            VStack {
+                EmptyView()
+            }
+            
+        }
+    }
+}
+
+struct PlayTaskView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        PlayTaskView()
+    }
+}
